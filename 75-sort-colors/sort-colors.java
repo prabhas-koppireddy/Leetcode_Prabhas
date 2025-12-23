488aa8f1f0f87040d0bd6356cee5b2b40270a34c
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2025-12-23 15:48:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (15.54%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -12,8 +12,5 @@ class Solution {
         for(int i=0;i<cnt0;i++) nums[i]=0;
         for(int i=cnt0;i<cnt0+cnt1;i++) nums[i]=1;
         for(int i=cnt0+cnt1;i<n;i++) nums[i]=2;
-        for(int i=0;i<n;i++){
-            System.out.print(nums[i]+" ");
-        }
     }
 }
